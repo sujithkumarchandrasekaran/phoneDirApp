474626a8075e961794d14d803cd7811e53a8ee1f
Sujith <sujithchandrasekaran@gmail.com>
2020-12-09 07:39:47 +0530

    External styling for hedaer

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import  "./Header.css"
 
 const Header = function () {
 
-    const headerStyle = {textAlign:'center', padding:20, background:'#000', color:'#fff',textTransform:'uppercase'};
+    // const headerStyle = {textAlign:'center', padding:20, background:'#000', color:'#fff',textTransform:'uppercase'};
     return (
 
-    <div style={headerStyle}>
+    <div className="header">
+    {/* <div style={headerStyle}></div> */}
       phone directory
     </div>
     )
