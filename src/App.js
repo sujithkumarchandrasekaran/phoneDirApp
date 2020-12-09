28ce715ddbb7515fe0231a1a25f08fa19eb462b0
Sujith <sujithchandrasekaran@gmail.com>
2020-12-09 08:08:59 +0530

    App CC added

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
 import react,  {Component} from 'react';
 import Header from "./Header.js";
+import "./App.css";
 
 class App extends Component {
   render () {
 
   return ( 
+  
     <div >
       <Header/>
-      <button>
+      {/* <button>
           Add
       </button>
       <div>
           <span>Name</span> <br/>
           <span>Phone Number</span>
-      </div>
+      </div> */}
       {/* <lable htmlfor="name">Name : </lable>
       <input id="name" type="text" placeholder="Type Here" defaultValue ={x+y}/> */}
-    </div> 
+
+        <div className="component-body-container">
+          <button className="custom-btn add-btn">Add</button>
+
+          <div className="grid-container heading-container">
+            <span className="grid-item name-heading">Name</span>
+            <span className="grid-item phone-heading">Phone</span>
+          </div>
+
+         </div> 
+    </div>
+
     
     ) ;
   }
